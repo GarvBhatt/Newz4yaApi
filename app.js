ef6fc74f1c88f9f27d3d6620234a70fa9c5be3ea
Garv <garvbhatt2085@gmail.com>
2023-10-31 03:35:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Listening to childern");
+});
+
+app.get("/news", async (req, res) => {
+  try {
+    const q=req.query.q;
+    const pageNo=req.query.pageNo;
+    let response=await axios.get(`https://newsapi.org/v2/everything?q=${q}&apiKey=fb0df5c5423b4963ae3f9f1ce1351384&page=${pageNo}&pageSize=8`);
+    response=response.data.articles;
+    res.send({response});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while fetching the newz.");
+  }
+});
+
+app.listen(80, () => {
+  console.log("Listening to childern");
+});
